a22d6693fadfe3f0931ffa98de794438b4635bba
Andreas Lövqvist <andreas.lovqvist@gmail.com>
2016-05-23 10:29:02 +0200

    Less code for dummy

--- www/controllers/home.js
+++ www/controllers/home.js
@@ -12,36 +12,26 @@ app.controller("home", ["$scope", "property", "homedb", "aboutdb", "workers", fu
     loadhome();
 
     // Dummy data
-    property.get({},function(data){if(data.length === 0){addProperties(data);}});
-    homedb.get({},function(data){if(data.length === 0){addHome(data);}});
-    aboutdb.get({},function(data){if(data.length === 0){addAbout(data)}});
-    workers.get({},function(data){if(data.length === 0){addWorkers(data)}});
+    property.get({},function(data){if(!data.length){addProperties(data);}});
+    homedb.get({},function(data){if(!data.length){addHome(data);}});
+    aboutdb.get({},function(data){if(!data.length){addAbout(data)}});
+    workers.get({},function(data){if(!data.length){addWorkers(data)}});
 
 	function addProperties(data){
-		console.log("No properties found");
 
 		function randomData() {
 
 			function randomNum(numLow, numHigh, startValue) {
-				if(!startValue) {
-					startValue = 0;
-				}
-
-				if(numLow === 0) {
-					var randomNum = Math.floor((Math.random() * (numHigh + 1) + numLow)) + startValue;
-					return randomNum;
-				}
-				else {
-				var randomNum = Math.floor((Math.random() * numHigh) + numLow) + startValue;
-				return randomNum;
-				}
+				return ((!numLow) ? Math.floor((Math.random() * (numHigh + 1) + numLow)):Math.floor((Math.random() * numHigh) + numLow) + ((!startValue) ? 0:startValue));
 			}
+
 			function randomType() {
 				var type = ["Apartment", "House"];
 				return type[randomNum(0,1)];
 			}
+
 			var adresses = ["Ramels väg", "Ernst", "Tessins väg", "Sergels väg", "Romlins väg", "Regementsgatan", "Nobelvägen", "Polvägen", "Limhamnsvägen"];
-			var data = {
+			return {
 				adress: adresses[randomNum(0, 10)],
 				zipcode: randomNum(1, 10000, 10000),
 				price: randomNum(1, 20000000, 700000),
@@ -52,12 +42,11 @@ app.controller("home", ["$scope", "property", "homedb", "aboutdb", "workers", fu
 				yardarea: randomNum(1, 1000, 50),
 				floors: randomNum(1, 10),
 				path: "img/objects/villa" + randomNum(1,11) + ".jpg"
-			}
-			return data;
+			};
 		}
 
 		var homes = [];
-		for (var i = 0; i < 124; i++) {
+		for (var i = 0; i < 164; i++) {
 			homes.push(randomData());
 
 		}
